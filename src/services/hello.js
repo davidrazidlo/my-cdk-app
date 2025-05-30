b788475045cad398f437f170785172e76e3d6bd8
davidrazidlo <davidrazidlo@gmail.com>
2025-05-29 22:19:59 -0500

    fix

--- src/services/hello.js
+++ src/services/hello.js
@@ -3,7 +3,7 @@ exports.main = async (event, context) => {
     statusCode: 201,
 
     body: JSON.stringify({
-      message: `Hello, I am reading from: ${process.env.TABLE_NAME} and my region is: ${process.env.AWS_REGION}`,
+      message: `Hello, I am reading from: ${process.env.SPACES_TABLE_NAME} and my region is: ${process.env.AWS_REGION}`,
     }),
   };
 };
