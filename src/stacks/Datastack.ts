ef5b2d8b54b759ee39d59377e05b3b3500ff9119
davidrazidlo <davidrazidlo@gmail.com>
2025-05-29 22:15:04 -0500

    asdf

--- src/stacks/Datastack.ts
+++ src/stacks/Datastack.ts
@@ -1,7 +1,19 @@
 import { Stack } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
+import { get } from "http";
+import { getSuffixFromStack } from "../infra/utils";
 
 export class DataStack extends Stack {
+  public readonly spacesTable: ITable;
+
   constructor(scope: any, id: string, props?: any) {
     super(scope, id);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.spacesTable = new Table(this, "SpacesTable", {
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      tableName: `SpacesTable-${suffix}`,
+    });
   }
 }
